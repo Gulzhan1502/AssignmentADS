a9da1ad2002b427bf512ef3f56d70aa9b9a2428e
gulzanutekina <gulzhan.utekina@gmail.com>
2023-04-08 22:33:18 +0600

    Task 2

---
+++ src/Average.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Average {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+        }
+        double ave = calculateAverage(arr);
+
+        System.out.println(ave);
+    }
+    public static double calculateAverage(int[] arr) {
+        int sum = sum(arr, arr.length - 1);
+        return (double) sum / arr.length;
+    }
+    private static int sum(int[] arr, int index) {
+        if (index == 0) {
+            return arr[index];
+        } else {
+            return arr[index] + sum(arr, index - 1);
+        }
+    }
+
+
+
+}
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
-public class Main {
+public class Main{
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = input.nextInt();
+            arr[i] = scan.nextInt();
         }
         int min = findMin(arr, n);
         System.out.println(min);
@@ -23,4 +23,5 @@ public class Main {
 
         return min;
     }
+
 }
